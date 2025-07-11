a37876127c13fd56af7019c5b7715ac92a634098
VEZINFINITY <vezinfinity@gmail.com>
2025-07-11 18:09:03 +0530

    Update config.py

--- config.py
+++ config.py
@@ -27,7 +27,7 @@ MONGO_DB_URI = getenv("MONGO_DB_URI", "mongodb+srv://knight4563:knight4563@clust
 DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 17000))
 
 # Chat id of a group for logging bot's activities
-LOGGER_ID = int(getenv("LOGGER_ID", -1002825379503))
+LOGGER_ID = int(getenv("LOGGER_ID", -1002643544937))
 
 # Get this value from @PURVI_HELP_BOT on Telegram by /id
 OWNER_ID = int(getenv("OWNER_ID", 7926944005))
